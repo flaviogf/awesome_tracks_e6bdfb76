d92398e5e4cec0b61de1b3b394cc49921a46362f
flaviogf <flavio.fernandes6@gmail.com>
2021-09-04 09:29:09 -0300

    feat: add temperature_by_city method

--- weather_service/lib/weather_service/open_weather_map_repository.rb
+++ weather_service/lib/weather_service/open_weather_map_repository.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module WeatherService
   class OpenWeatherMapRepository
+    def initialize(client:)
+      @client = client
+    end
+
+    def temperature_by_city(city)
+      response = @client.get("http://api.openweathermap.org/data/2.5/weather?q=i#{city}&appid=b77e07f479efe92156376a8b07640ced")
+
+      return if response.status != 200
+
+      body = JSON.parse(response.body)
+
+      Float(body.dig('main', 'temp'))
+    end
   end
 end
