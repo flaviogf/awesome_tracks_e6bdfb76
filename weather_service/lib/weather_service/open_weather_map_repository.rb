e533003dc6e1efc799716a37004164b3b87f1103
flaviogf <flavio.fernandes6@gmail.com>
2021-09-04 10:47:29 -0300

    feat: handle error at temperature_by_city method

--- weather_service/lib/weather_service/open_weather_map_repository.rb
+++ weather_service/lib/weather_service/open_weather_map_repository.rb
@@ -4,18 +4,17 @@ require 'json'
 
 module WeatherService
   class OpenWeatherMapRepository
-    def initialize(client:)
+    def initialize(client:, logger:)
       @client = client
+      @logger = logger
     end
 
     def temperature_by_city(city)
       response = @client.get("http://api.openweathermap.org/data/2.5/weather?q=i#{city}&appid=b77e07f479efe92156376a8b07640ced")
-
-      return if response.status != 200
-
       body = JSON.parse(response.body)
-
       Float(body.dig('main', 'temp'))
+    rescue StandardError => e
+      @logger.error(e)
     end
   end
 end
