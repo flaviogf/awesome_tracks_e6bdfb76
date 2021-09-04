413a47552ea62562e100022760989324db115de1
flaviogf <flavio.fernandes6@gmail.com>
2021-09-04 13:29:04 -0300

    feat: add require open_weather_map_repository_cached

--- weather_service/lib/weather_service.rb
+++ weather_service/lib/weather_service.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'weather_service/open_weather_map_repository'
+require 'weather_service/open_weather_map_repository_cached'
 
 module WeatherService
   module_function
