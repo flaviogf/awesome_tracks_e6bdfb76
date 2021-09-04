ed3a8369e52c516d7be706eb877d7aa585459aba
flaviogf <flavio.fernandes6@gmail.com>
2021-09-04 13:28:40 -0300

    test: add open_weather_map_repository_cached_spec

---
+++ weather_service/spec/open_weather_map_repository_cached_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module WeatherService
+  RSpec.describe OpenWeatherMapRepositoryCached do
+    describe '.temperature_by_city' do
+      context 'when pass city' do
+        subject!(:service) do
+          described_class.new(repository: repository, cache: cache, logger: logger).temperature_by_city(city)
+        end
+
+        let(:repository) { double('repository', temperature_by_city: temperature) }
+
+        let(:cache) { double('cache', set: 'OK', get: nil) }
+
+        let(:logger) { double('logger', error: nil) }
+
+        let(:city) { Faker::Address.city }
+
+        let(:temperature) { 294.46 }
+
+        it { is_expected.to be_a(Float) }
+
+        context 'when something goes wrong' do
+          let(:cache) do
+            cache = double('cache')
+            allow(cache).to receive(:get).and_raise(StandardError)
+            cache
+          end
+
+          it { expect(logger).to have_received(:error).once }
+        end
+
+        context 'when cache was not hit' do
+          it { expect(repository).to have_received(:temperature_by_city).with(city).once }
+
+          it { expect(cache).to have_received(:get).with(city).once }
+
+          it { expect(cache).to have_received(:set).with(city, temperature, { ex: 60 * 30 }).once }
+        end
+
+        context 'when cache was hit' do
+          let(:cache) { double('cache', set: 'OK', get: temperature) }
+
+          it { expect(repository).not_to have_received(:temperature_by_city) }
+
+          it { expect(cache).to have_received(:get).with(city).once }
+
+          it { expect(cache).not_to have_received(:set) }
+        end
+      end
+    end
+  end
+end
