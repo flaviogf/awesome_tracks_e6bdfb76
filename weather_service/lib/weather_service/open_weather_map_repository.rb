1e860f7d84075af5b6e000d71df6ce3bde0c0b2a
flaviogf <flavio.fernandes6@gmail.com>
2021-09-05 09:28:19 -0300

    refactor: extract API_KEY constant

--- weather_service/lib/weather_service/open_weather_map_repository.rb
+++ weather_service/lib/weather_service/open_weather_map_repository.rb
@@ -4,13 +4,15 @@ require 'json'
 
 module WeatherService
   class OpenWeatherMapRepository
+    API_KEY = ENV.fetch('OPEN_WEATHER_MAP_API_KEY', '').freeze
+
     def initialize(client:, logger:)
       @client = client
       @logger = logger
     end
 
     def temperature_by_city(city)
-      response = @client.get("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=metric&appid=b77e07f479efe92156376a8b07640ced")
+      response = @client.get("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=metric&appid=#{API_KEY}")
       body = JSON.parse(response.body)
       Float(body.dig('main', 'temp'))
     rescue StandardError => e
