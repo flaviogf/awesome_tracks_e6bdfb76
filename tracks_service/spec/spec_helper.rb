095f94f6c4f89eddbaa3870aec6f943e434150c0
flaviogf <flavio.fernandes6@gmail.com>
2021-09-05 09:49:54 -0300

    test: add rspec

---
+++ tracks_service/spec/spec_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'pry'
+require 'tracks_service'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
