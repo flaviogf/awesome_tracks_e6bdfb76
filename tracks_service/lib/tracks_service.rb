6f969c6cae96f092ba67f8a3ac14bb6e2cc7c822
flaviogf <flavio.fernandes6@gmail.com>
2021-09-06 13:59:25 -0300

    feat: add tracks_service module

--- tracks_service/lib/tracks_service.rb
+++ tracks_service/lib/tracks_service.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+require 'base64'
+require 'faraday'
+require 'json'
+require 'tracks_service/result'
+require 'tracks_service/spotify_track_repository'
+
+module TracksService
+end
