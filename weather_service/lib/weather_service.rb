d3e9b2735c8b18ff0a45c6480192314469617023
flaviogf <flavio.fernandes6@gmail.com>
2021-09-06 11:13:57 -0300

    feat: change returned value from temperature method

--- weather_service/lib/weather_service.rb
+++ weather_service/lib/weather_service.rb
@@ -9,33 +9,37 @@ require 'weather_service/open_weather_map_repository'
 require 'weather_service/open_weather_map_cached_repository'
 
 module WeatherService
-  module_function
+  class << self
+    include Result::Methods
 
-  def temperature(request)
-    $stdout.sync = true
+    def temperature(request)
+      cached_repository.temperature_by_city(request.city)
+    rescue StandardError => e
+      logger.error(e)
 
-    cached_repository.temperature_by_city(request.city)
-  rescue StandardError => e
-    logger.error(e)
-  end
+      failure(e.message)
+    end
 
-  def cached_repository
-    OpenWeatherMapCachedRepository.new(repository: repository, cache: cache, logger: logger)
-  end
+    private
 
-  def repository
-    OpenWeatherMapRepository.new(client: client, logger: logger)
-  end
+    def cached_repository
+      OpenWeatherMapCachedRepository.new(repository: repository, cache: cache, logger: logger)
+    end
 
-  def client
-    Faraday.new(request: { timeout: 5 }) { |f| f.use Faraday::Response::RaiseError }
-  end
+    def repository
+      OpenWeatherMapRepository.new(client: client, logger: logger)
+    end
 
-  def logger
-    Logger.new
-  end
+    def client
+      Faraday.new(request: { timeout: 5 }) { |f| f.use Faraday::Response::RaiseError }
+    end
 
-  def cache
-    Redis.new(host: 'cache', timeout: 5)
+    def logger
+      Logger.new
+    end
+
+    def cache
+      Redis.new(host: 'cache', timeout: 5)
+    end
   end
 end
--- weather_service/spec/weather_service_spec.rb
+++ weather_service/spec/weather_service_spec.rb
@@ -3,25 +3,27 @@
 RSpec.describe WeatherService do
   describe '.temperature' do
     context 'when cache does not connect' do
-      subject! do
-        allow(described_class).to receive(:logger).and_return(logger)
-        allow(described_class).to receive(:cache).and_raise(StandardError)
-        described_class.temperature(request)
-      end
-
       let(:request) { Request.new(city) }
 
       let(:city) { Faker::Address.city }
 
-      let(:logger) do
-        logger = double('logger')
+      let(:logger) { double('logger') }
+
+      before do
         allow(logger).to receive(:error)
-        logger
+        allow(described_class).to receive(:logger).and_return(logger)
+        allow(described_class).to receive(:cache).and_raise(StandardError.new('oops'))
       end
 
-      it { is_expected.to be_nil }
+      it 'returns failed result' do
+        result = described_class.temperature(request)
+        expect(result.failure?).to be_truthy
+      end
 
-      it { expect(logger).to have_received(:error).once }
+      it 'calls logger to record an error' do
+        described_class.temperature(request)
+        expect(logger).to have_received(:error).once
+      end
     end
   end
 end
