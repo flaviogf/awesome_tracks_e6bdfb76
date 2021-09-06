835d3114fa42060471f2fcf1f9b1bc7c55d2cf68
flaviogf <flavio.fernandes6@gmail.com>
2021-09-06 13:58:36 -0300

    feat: add result

---
+++ tracks_service/lib/tracks_service/result.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Result
+  module Methods
+    module_function
+
+    def success(value)
+      Result.new(success: true, value: value)
+    end
+
+    def failure(error)
+      Result.new(success: false, error: error)
+    end
+  end
+
+  class Result
+    def initialize(success:, error: nil, value: nil)
+      @success = success
+      @error = error
+      @value = value
+    end
+
+    def success?
+      @success
+    end
+
+    def failure?
+      !@success
+    end
+
+    def error
+      raise StandardError, 'error is nil' if @error.nil?
+
+      @error
+    end
+
+    def value
+      raise StandardError, 'value is nil' if @value.nil?
+
+      @value
+    end
+  end
+end
