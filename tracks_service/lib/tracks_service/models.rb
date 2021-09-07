674140d2c3fa503ed80ce35b4813573fe79d687a
flaviogf <flavio.fernandes6@gmail.com>
2021-09-07 13:03:09 -0300

    feat: add Models

---
+++ tracks_service/lib/tracks_service/models.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'tracks_service/models/album'
+require 'tracks_service/models/artist'
+require 'tracks_service/models/track'
+
+module TracksService
+  module Models
+  end
+end
