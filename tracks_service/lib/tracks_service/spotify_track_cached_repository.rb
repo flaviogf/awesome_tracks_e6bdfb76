cfb421408ef6ac292ef26f538cb90c6cbedbbf20
flaviogf <flavio.fernandes6@gmail.com>
2021-09-11 13:18:22 -0300

    feat: add expected behaviors at SpotifyTrackCachedRepository

--- tracks_service/lib/tracks_service/spotify_track_cached_repository.rb
+++ tracks_service/lib/tracks_service/spotify_track_cached_repository.rb
@@ -4,13 +4,30 @@ module TracksService
   class SpotifyTrackCachedRepository
     include Result::Methods
 
-    def initialize(repository:)
+    def initialize(repository:, cache:, logger:)
       @repository = repository
+      @cache = cache
+      @logger = logger
     end
 
     def track_by_theme(theme)
+      track_as_json = @cache.get(theme)
+
+      return success(Models::Track.from_json(track_as_json)) unless track_as_json.nil?
+
       result = @repository.track_by_theme(theme)
-      success(result.value)
+
+      return failure("could not get track for theme: #{theme}") if result.failure?
+
+      track = result.value
+
+      @cache.set(theme, track.to_json, ex: 60 * 30)
+
+      success(track)
+    rescue StandardError => e
+      @logger.error(e)
+
+      failure("could not get track for theme: #{theme}")
     end
   end
 end
