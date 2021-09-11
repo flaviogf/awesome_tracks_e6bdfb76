3f3a8c8ab16ea41ab77ccf50a77591915d856a88
flaviogf <flavio.fernandes6@gmail.com>
2021-09-11 12:55:17 -0300

    feat: add Album.from_json method

--- tracks_service/lib/tracks_service/models/album.rb
+++ tracks_service/lib/tracks_service/models/album.rb
@@ -3,6 +3,18 @@
 module TracksService
   module Models
     class Album
+      def self.from_json(json)
+        json = json.to_json if json.is_a?(Hash)
+        json = JSON.parse(json)
+
+        new(
+          json.fetch('id'),
+          json.fetch('name'),
+          json.fetch('url'),
+          json.fetch('image_url')
+        )
+      end
+
       def initialize(id, name, url, image_url)
         @id = id
         @name = name
