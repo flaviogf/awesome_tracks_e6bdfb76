ed36583cd78f0f713f5ec93bb3720f1b2391670c
flaviogf <flavio.fernandes6@gmail.com>
2021-09-18 08:45:15 -0300

    feat: implement GRPCTrackRepository

--- awesome_tracks_api/lib/awesome_tracks_api/repositories/grpc_track_repository.rb
+++ awesome_tracks_api/lib/awesome_tracks_api/repositories/grpc_track_repository.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
+this_dir = File.expand_path(__dir__)
+
+$LOAD_PATH.unshift(this_dir) unless $LOAD_PATH.include?(this_dir)
+
+require 'tracks_service_pb'
+require 'tracks_service_services_pb'
+
 module AwesomeTracksApi
   module Repositories
     class GRPCTrackRepository
       include Result::Methods
 
       def track(request)
-        success(request)
+        stub = TracksService::Service::Stub.new('tracks_service:3000', :this_channel_is_insecure)
+
+        result = stub.track(TracksService::TrackRequest.new(theme: request.fetch(:theme)))
+
+        return failure(result.error) unless result.success
+
+        success(result.value)
+      rescue StandardError => e
+        failure(e.error)
       end
     end
   end
