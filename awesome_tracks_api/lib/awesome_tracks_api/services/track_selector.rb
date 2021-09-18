b613cef7f3aac5c869f23f570b46b3e9e1b2d31b
flaviogf <flavio.fernandes6@gmail.com>
2021-09-18 08:32:12 -0300

    feat: add method call at TrackSelector

--- awesome_tracks_api/lib/awesome_tracks_api/services/track_selector.rb
+++ awesome_tracks_api/lib/awesome_tracks_api/services/track_selector.rb
@@ -3,6 +3,26 @@
 module AwesomeTracksApi
   module Services
     class TrackSelector
+      include Result::Methods
+
+      def self.call(request, track_repository: Repositories::GRPCTrackRepository.new)
+        new(request, track_repository).call
+      end
+
+      def initialize(request, track_repository)
+        @request = request
+        @track_repository = track_repository
+      end
+
+      def call
+        result = @track_repository.track(theme: 'rock')
+
+        return failure('could not get track') unless result.success
+
+        track = result.value
+
+        success(track)
+      end
     end
   end
 end
