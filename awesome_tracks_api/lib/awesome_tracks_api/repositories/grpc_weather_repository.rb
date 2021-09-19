f58b1570a617c14d5f0171ef6965e2078d59cbb1
flaviogf <flavio.fernandes6@gmail.com>
2021-09-19 08:52:31 -0300

    feat: implement GRPCWeatherRepository

--- awesome_tracks_api/lib/awesome_tracks_api/repositories/grpc_weather_repository.rb
+++ awesome_tracks_api/lib/awesome_tracks_api/repositories/grpc_weather_repository.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
+this_dir = File.expand_path(__dir__)
+
+$LOAD_PATH.unshift(this_dir) unless $LOAD_PATH.include?(this_dir)
+
+require 'weather_service_pb'
+require 'weather_service_services_pb'
+
 module AwesomeTracksApi
   module Repositories
     class GRPCWeatherRepository
       include Result::Methods
 
       def temperature(request)
-        success(request)
+        stub = WeatherService::Service::Stub.new('weather_service:3000', :this_channel_is_insecure)
+
+        result = stub.temperature(WeatherService::TemperatureRequest.new(city: request.fetch(:city)))
+
+        return failure(result.error) unless result.success
+
+        success(result.value)
+      rescue StandardError => e
+        failure(e.message)
       end
     end
   end
